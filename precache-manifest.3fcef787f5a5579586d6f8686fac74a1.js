4aa34886fcce7ce389ecd8b0e7cb1d66c6df8965
Aline Swinkels <alineswinkels@mbp-van-aline.home>
2023-09-22 15:25:09 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3fcef787f5a5579586d6f8686fac74a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc2abf6d5f1bfb0f5801",
+    "url": "/PhotographyPortfolio/app/static/css/main.82c4953b.chunk.css"
+  },
+  {
+    "revision": "cc2abf6d5f1bfb0f5801",
+    "url": "/PhotographyPortfolio/app/static/js/main.cc2abf6d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/PhotographyPortfolio/app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/PhotographyPortfolio/app/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "76319c8739581a24d47c",
+    "url": "/PhotographyPortfolio/app/static/js/runtime~main.76319c87.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/PhotographyPortfolio/app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "40cd99bfe174bb7d4f67d8f382892de2",
+    "url": "/PhotographyPortfolio/app/index.html"
+  }
+];
