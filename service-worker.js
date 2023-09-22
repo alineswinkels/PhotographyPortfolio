be0bd996dccb830a29cb8f5182a2085d85116606
Aline Swinkels <alineswinkels@mbp-van-aline.home>
2023-09-22 15:34:34 +0200

    Deploy React app to own domain

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PhotographyPortfolio/precache-manifest.559aac4e4e6a3848cf585eeae016c17f.js"
+  "/precache-manifest.bd8768b58101c0dd1f064db422815122.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/PhotographyPortfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
