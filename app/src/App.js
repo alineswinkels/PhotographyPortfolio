d34b25757eb8167ded136cd92d58e6cae7e0d01f
Aline Swinkels <alineswinkels@mbp-van-aline.home>
2023-09-28 13:02:49 +0200

    delete learn react

--- app/src/App.js
+++ app/src/App.js
@@ -6,18 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="gs://photosbyaline-portfolio.appspot.com/Scherm­afbeelding 2023-02-11 om 23.31.52.png" className="App-logo" alt="logo" />
         <p>
-          testing
+          Portfolio
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
