85976900aef4ce96c0a21fe5fab5742283fd3973
Aline Swinkels <alineswinkels@mbp-van-aline.home>
2025-06-25 17:01:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95a904ee62c8503a232f196f202baa98.js"
+  "/precache-manifest.f36c455e32c3c11e60efc57e099da969.js"
 );
 
 workbox.clientsClaim();
