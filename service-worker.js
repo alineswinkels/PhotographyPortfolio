294e2dfb6cfaaf1dcaa487ac350e7bed7d78b3c3
Aline Swinkels <alineswinkels@mbp-van-aline.home>
2025-06-25 17:03:27 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f36c455e32c3c11e60efc57e099da969.js"
+  "www.alineswinkels.nl/precache-manifest.b74df649645585ce2426bc43b189170a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("www.alineswinkels.nl/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
